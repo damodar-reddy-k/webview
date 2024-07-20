0f9bee341362064812b3e82437e40ea2f845bef7
Damodar Reddy K <116241273+damodar-reddy-k@users.noreply.github.com>
2024-07-20 11:24:12 +0000

    update : added basic structure

---
+++ my-portfolio-website/src/components/Hero.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Hero() { // Replace 'Hero' with the component name
+  return (
+    <div>
+      {/* Component content will go here */}
+    </div>
+  );
+}
+
+export default Hero; // Replace 'Hero' with the component name 
