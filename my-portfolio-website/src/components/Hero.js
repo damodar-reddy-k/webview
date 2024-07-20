54c8dd10e11b85868f1f99ab8022935061e6d0a5
Damodar Reddy K <116241273+damodar-reddy-k@users.noreply.github.com>
2024-07-20 12:04:34 +0000

    hero component

--- my-portfolio-website/src/components/Hero.js
+++ my-portfolio-website/src/components/Hero.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import './Hero.css'; // We'll create this CSS file later
 
-function Hero() { // Replace 'Hero' with the component name
+function Hero() {
   return (
-    <div>
-      {/* Component content will go here */}
-    </div>
+    <section className="hero">
+      <div className="hero-content">
+        <h1>Damodar Reddy K</h1> 
+        <h2>Aspiring Actuarial Data Scientist</h2> 
+      </div>
+    </section>
   );
 }
 
-export default Hero; // Replace 'Hero' with the component name 
+export default Hero;
